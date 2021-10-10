38e580db87974a21a4fbaaa84dccd3cbfe3b7f9e
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2021-10-10 15:58:48 +0700

    route fixing

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-    return view('index');
+    return view('component.container');
 })->name('home');
 Route::get('/dashboard', 'IndexController@index');
 Auth::routes();
